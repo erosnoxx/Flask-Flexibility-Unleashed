50c518935dbe0523b146f5a922e49db3edbacc9f
Eros <garotoslixo@gmail.com>
2023-11-26 14:56:42 -0300

    cep finder added

--- app/models/users.py
+++ app/models/users.py
@@ -6,11 +6,13 @@ class User(db.Model):
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(255), nullable=False)
 
+
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     address = db.Column(db.String(255), nullable=False)
     city = db.Column(db.String(32), nullable=False)
+    neighborhood = db.Column(db.String(255), nullable=False)
     state = db.Column(db.String(18), nullable=False)
     zipcode = db.Column(db.Integer, nullable=False)
     country = db.Column(db.String(6), nullable=False, default='Brazil')
--- app/services/cep_finder.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = 'https://viacep.com.br/ws/'
-
-def get_cep(cep):
-    r = requests.get(url + cep + '/json/')
-    return r.json()
